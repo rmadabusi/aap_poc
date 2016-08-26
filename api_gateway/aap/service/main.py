45a7dc7559a5083652a972b9b30a507aa2480dab
Aravind <Aravind>
2016-08-26 20:03:11 +0530

    Gateway services for Usage metrics Details and color details

--- api_gateway/aap/service/main.py
+++ api_gateway/aap/service/main.py
@@ -1,7 +1,8 @@
 from __future__ import division
-from flask import Flask
+from flask import Flask, request
 import json
-from api_gateway.aap.service.implementations.main_services import get_status, get_family_details
+from api_gateway.aap.service.implementations.main_services import get_status, get_family_details, get_colors_details, pet_usage_metrics
+from urllib import unquote
 
 __author__ = "Treselle"
 
@@ -19,6 +20,28 @@ def api_status():
 def api_family_details(id):
     result = get_family_details(id)
     return result
+
+#For getting color details based on Pet color ids
+@app.route('/pets/colors/<id>', methods=["GET"])
+def api_color_details(id):
+    result = get_colors_details(id)
+    return result
+
+#For Tracking Usage metrics Details
+@app.route("/pets/usage_metrics", methods=["GET", "POST"])
+def dsn_api_entity_find_raw():
+    params = {}
+    if request.method == "GET":
+        try:
+            params = json.loads(request.args.get("params", "{}"))
+        except:
+            token_remove_params = (request.query_string.replace ("&", " ").partition("token"))[0]
+            params = json.loads(unquote(token_remove_params)[7:])
+    elif request.method == "POST":
+        params = json.loads(request.data).get("params", "{}")
+    entities_recs = pet_usage_metrics(params)
+    return entities_recs
+
 
 #Main Function
 if __name__ == '__main__':
